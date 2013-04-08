6fd53349baa580fc50b80ba7dbeac794a975d2ab
kilroySoft <info@kilroysoft.ch>
2013-04-09 01:36:16 +0200

    Initial commit
    First launch version alpha 0

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to aloyscor
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject aloyscore "0.1.0-ALPHA0"
+  :description "Web stack framework"
+  :url "https://github.com/kilroysoft/aloyscore"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :main aloyscore.core)
